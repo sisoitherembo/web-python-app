00888765e3bb7ac428107f4a0bf7876646cc6532
sisoitherembo <dmitri20023zarubo@gmail.com>
2022-06-16 11:48:17 +0300

    a

--- webpythonapp/webpythonapp/settings.py
+++ webpythonapp/webpythonapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY") 
+SECRET_KEY = os.environ.get("SECRET_KEY", default=r'django-insecure-oldwf9$exg*d45m5z%19h^-ya+uk*a17e2+7+2%%-#0o1!c5-%') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
